7b5a06698918bb7ad0eecc9ed12e65917a3168b0
Alexander Plotnikov <Appplotnikov2019@gmail.com>
2023-05-22 14:23:46 +0300

    Correct version

--- Homework Перегрузка операторов N2/main.cpp
+++ Homework Перегрузка операторов N2/main.cpp
@@ -6,38 +6,6 @@ private:
 	int numerator_;
 	int denominator_;
 public:
-	Fraction(int numerator, int denominator)
-	{
-		numerator_ = numerator;
-		denominator_ = denominator;
-	}
-	static Fraction reduction(Fraction p)
-	{
-		int a;
-		if (p.numerator_ > p.denominator_)
-		{
-			a = p.denominator_;
-		}
-		else
-		{
-			a = p.numerator_;
-		}
-		int i = 2;
-		while (i<=a)
-		{
-			if (p.numerator_ % i == 0 && p.denominator_ % i == 0)
-			{
-				p.numerator_ /= i;
-				p.denominator_ /= i;
-				i = 2;
-			}
-			else
-			{
-				i++;
-			}
-		}
-		return p;
-	}
 	friend std::ostream& operator<<(std::ostream& couter, const Fraction& p)
 	{
 		if (p.numerator_ == p.denominator_)
@@ -48,7 +16,10 @@ public:
 		{
 			couter << p.numerator_;
 		}
-		couter << p.numerator_ << "/" << p.denominator_;
+		else
+		{
+			couter << p.numerator_ << "/" << p.denominator_;
+		}
 		return couter;
 	}
 	Fraction operator+(Fraction p)
@@ -77,21 +48,50 @@ public:
 	}
 	Fraction operator-() { return Fraction( - numerator_,denominator_); }
 	Fraction operator++() { numerator_ += denominator_; return Fraction(numerator_,denominator_); }
-	Fraction operator++(int) { numerator_ += denominator_; return Fraction(numerator_, denominator_); }
-	Fraction operator--() { numerator_ -= denominator_; return Fraction(numerator_, denominator_); }
-	Fraction operator--(int) { numerator_ -= denominator_; return Fraction(numerator_, denominator_); }
-	static Fraction reduction1(Fraction p)
+	Fraction operator++(int)
 	{
-		if (p.numerator_ / p.denominator_ >= 0)
+		Fraction temp = *this;
+		++* this;
+		return temp;
+	}
+	Fraction operator--() { numerator_ -= denominator_; return Fraction(numerator_, denominator_); }
+	Fraction operator--(int)
+	{
+		Fraction temp = *this;
+		--* this;
+		return temp;
+	}
+	Fraction(int numerator, int denominator)
+	{
+		numerator_ = numerator;
+		denominator_ = denominator;
+	}
+	static Fraction reduction(Fraction p)
+	{
+		int a;
+		if (abs(p.numerator_) > abs(p.denominator_))
 		{
-			return reduction(p);
+			a = abs(p.denominator_);
 		}
 		else
 		{
-			p.numerator_ = abs(p.numerator_);
-			p.denominator_ = abs(p.denominator_);
-			return -reduction(p);
+			a = abs(p.numerator_);
 		}
+		int i = 2;
+		while (i <= a)
+		{
+			if (p.numerator_ % i == 0 && p.denominator_ % i == 0)
+			{
+				p.numerator_ /= i;
+				p.denominator_ /= i;
+				i = 2;
+			}
+			else
+			{
+				i++;
+			}
+		}
+		return p;
 	}
 };
 
@@ -108,13 +108,15 @@ int main()
 	std::cin >> b1;
 	Fraction p1(a, b);
 	Fraction p2(a1, b1);
-	std::cout << p1 << " + " << p2 << " = " << Fraction::reduction1(p1 + p2) << std::endl;
-	std::cout << p1 << " - " << p2 << " = " << Fraction::reduction1(p1 - p2) << std::endl;
-	std::cout << p1 << " * " << p2 << " = " << Fraction::reduction1(p1 * p2) << std::endl;
-	std::cout << p1 << " / " << p2 << " = " << Fraction::reduction1(p1 / p2) << std::endl;
+	std::cout << p1 << " + " << p2 << " = " << Fraction::reduction(p1 + p2) << std::endl;
+	std::cout << p1 << " - " << p2 << " = " << Fraction::reduction(p1 - p2) << std::endl;
+	std::cout << p1 << " * " << p2 << " = " << Fraction::reduction(p1 * p2) << std::endl;
+	std::cout << p1 << " / " << p2 << " = " << Fraction::reduction(p1 / p2) << std::endl;
 	std::cout << "++" << p1 << " * " << p2 << " = ";
-	std::cout << Fraction::reduction1((++p1) * p2) << std::endl;
+	std::cout << Fraction::reduction((++p1) * p2) << std::endl;
+	std::cout << "Value of first fraction: " << Fraction::reduction(p1) << std::endl;
 	std::cout << p1 << "--" << " * " << p2 << " = ";
-	std::cout << Fraction::reduction1((p1--) * p2) << std::endl;
+	std::cout << Fraction::reduction((p1--) * p2) << std::endl;
+	std::cout << "Value of first fraction: " << Fraction::reduction(p1) << std::endl;
 	return 0;
 }
